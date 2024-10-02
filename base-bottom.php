517bc2bf65c6f11c58b515b36c95221f5558f7bd
“CyberArchitect777” <169656116+CyberArchitect777@users.noreply.github.com>
2024-10-03 00:08:43 +0100

    refactor: split index.php into two templates for use on all pages

---
+++ base-bottom.php
@@ -0,0 +1,9 @@
+<footer>
+        <p class="py-2 my-auto text-center">Created by Barrie Millar (2024)</p>
+    </footer>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+        crossorigin="anonymous"></script>
+</body>
+
+</html>
--- index.php
+++ index.php
@@ -1,56 +1,11 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" href="assets/css/style.css">
-    <title>MyMeds</title>
-</head>
-
-<body>
-    <header>
-        <nav class="navbar navbar-expand-lg" data-bs-theme="dark">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="#">MyMeds</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                    aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-                        <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="#">Home</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Login</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Register</a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
-    </header>
+<?php include "base-top.php"; ?>
     <main>
         <h1 class="main-text">Welcome to MyMeds</h1>
         <h2 class="main-text">Your companion for managing and tracking your essential medications effortlessly</h2>
-        <p class="main-text">Please register or sign-in to continue</p>
+        <p class="main-text">Please sign-in or register to continue</p>
         <section class="d-flex justify-content-center mt-5">
             <a class="blue-button me-3" href="#">Login</a>
             <a class="blue-button ms-3" href="#">Register</a>
         </section>
     </main>
-    <footer>
-        <p class="py-2 my-auto text-center">Created by Barrie Millar (2024)</p>
-    </footer>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
-        crossorigin="anonymous"></script>
-</body>
-
-</html>
+<?php include "base-bottom.php"; ?>
