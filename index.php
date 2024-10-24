c86ee808c19e5d8dce267bb89c2e0594f7b74a78
“CyberArchitect777” <169656116+CyberArchitect777@users.noreply.github.com>
2024-10-24 14:16:33 +0100

    build: stop errors displaying on the screen in deployment

--- index.php
+++ index.php
@@ -1,4 +1,10 @@
-<?php 
+<?php
+
+// Ensure errors are not displayed on the page, but can be seen in logs
+ini_set('display_errors', 0);
+ini_set('log_errors', 1);
+error_reporting(E_ALL);
+
 $pagename = "index"; 
 include "base-top.php"; 
 ?>
