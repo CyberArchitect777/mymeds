a9a87453b4860f1b432e2d1cc614932378d7a04b
“CyberArchitect777” <169656116+CyberArchitect777@users.noreply.github.com>
2024-10-29 19:48:52 +0000

    chore: change session token to user_id

--- login-process.php
+++ login-process.php
@@ -1,7 +1,5 @@
 <?php
 
-include "base-top.php";
-
 try {
 
     // Connect to the database
@@ -20,6 +18,7 @@ try {
     // Set an error variable to 0 to assume no initial errors
 
     $error_code = 0;
+    $message = "";
 
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
@@ -32,24 +31,28 @@ try {
             
             // Pull the password for this user account from the database
 
-            $account_pull = $pdo->prepare("SELECT password FROM users WHERE username = :passed_username");  
+            $account_pull = $pdo->prepare("SELECT user_id, password FROM users WHERE username = :passed_username");  
             $account_pull->execute(["passed_username" => $passed_username]);
-            $password_hash = $account_pull->fetchColumn();
+
+            while ($row = $account_pull->fetch(PDO::FETCH_ASSOC)) {
+                $password_hash = $row['password'];
+                $user_id = $row['user_id'];
+            }
 
             // Check if login information given matches what is in the database
 
             if (password_verify($passed_password, $password_hash )) {
-                echo "<p class='text-white'>Correct user account detected</p>";
                 // Re-hash password with new default algorithm if it's needed
                 if (password_needs_rehash($password_hash, PASSWORD_DEFAULT)) {
                     $new_password_hash = password_hash($passed_password, PASSWORD_DEFAULT);
                     $insert_query = $pdo->prepare("UPDATE users SET password = :passed_password WHERE username = :passed_username;");
                     $insert_query->execute( ["passed_username"=> $passed_username, "passed_password" => $passed_password]);
-                    session_start();
-                    $_SESSION['username'] = $passed_username;
-                }  
+                }
+                session_start();
+                $_SESSION["user_id"] = $user_id;
+                $message = "<p class='text-white'>Correct user account detected: " . (string)$user_id . "</p>";
             } else {
-                echo "<p class='text-white'>Incorrect login details</p>";
+                $message = "<p class='text-white'>Incorrect login details</p>";
             }
         }
     }
@@ -59,5 +62,9 @@ try {
 } catch (PDOException $e) {
     echo "Connection failed: " . $e->getMessage();
 }
+
+include "base-top.php";
+
+echo $message;
 
 include "base-bottom.php";
