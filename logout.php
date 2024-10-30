e365af806be82d70831dc29607ca166ad5aea883
“CyberArchitect777” <169656116+CyberArchitect777@users.noreply.github.com>
2024-10-30 18:37:33 +0000

    style: limit size of forms to 500px

--- logout.php
+++ logout.php
@@ -8,7 +8,7 @@ include "base-top.php";
 <p class="main-text">Are you sure you want to logout?</p>
 <form id="login" method="POST" action="logout-process.php">
     <div class="d-flex justify-content-center mt-5">
-        <input class="blue-button me-4" type="submit" value="Logout">
+        <input class="blue-button" type="submit" value="Logout">
     </div>
 </form>
 
