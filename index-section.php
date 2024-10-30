7c59fb943f7db6cc92e9e23b12c370dc19ca0e27
“CyberArchitect777” <169656116+CyberArchitect777@users.noreply.github.com>
2024-10-30 18:32:55 +0000

    feat: update home screen to show different options depending on login status

--- index-section.php
+++ index-section.php
@@ -1,9 +1,20 @@
 <main>
     <h1 class="main-text">Welcome to MyMeds</h1>
     <h2 class="main-text">Your companion for managing and tracking your essential medications effortlessly</h2>
-    <p class="main-text">Please sign-in or register to continue</p>
+    <p class="main-text">Please choose an option below to continue</p>
     <section class="d-flex justify-content-center mt-5">
-        <a class="blue-button me-3" href="#">Login</a>
-        <a class="blue-button ms-3" href="register.php">Register</a>
+        <?php 
+        if (isset($_SESSION["user_id"])) {
+            echo '
+            <a class="blue-button me-3" href="medhub.php">MedHub</a>
+            <a class="blue-button ms-3" href="logout.php">Logout</a>
+            ';
+        } else {
+            echo '
+            <a class="blue-button me-3" href="login.php">Login</a>
+            <a class="blue-button ms-3" href="register.php">Register</a>
+            ';
+        }
+        ?>
     </section>
 </main>
