8d84677f36778af12d1163599dda609593f124a5
“CyberArchitect777” <169656116+CyberArchitect777@users.noreply.github.com>
2024-10-31 18:50:38 +0000

    feat: create friendly outcome for user registration process

--- register-process.php
+++ register-process.php
@@ -1,10 +1,14 @@
 <?php
 
+$pagename = "register";
 include "session-code.php";
-include "base-top.php";
 include "errorlog.php";
 
 try {
+
+    // HTML Message display
+
+    $message = "";
 
     // Connect to the database
 
@@ -27,6 +31,7 @@ try {
             
             $confirm_password = $_POST["confirm-password"];
             if ($new_password !== $confirm_password) {
+                include "base-top.php";
                 echo "<p id='register-alert'>Passwords don't match. Please try again</p>";
                 include "register-section.php";
             } else {
@@ -38,6 +43,7 @@ try {
                 $username_count = $username_check->fetchColumn();
 
                 if ($username_count > 0) {
+                    include "base-top.php";
                     echo "<p class='text-white' id='register-alert'>Username already exists</p>";
                     include "register-section.php";
                 } else {
@@ -45,15 +51,27 @@ try {
                     $hashed_password = password_hash($new_password, PASSWORD_DEFAULT); 
                     $insert_query = $pdo->prepare("INSERT INTO users (username, password) VALUES (:new_username, :hashed_password);");
                     $insert_query->execute( ["new_username"=> $new_username, "hashed_password" => $hashed_password]);
-                    echo "<p class='text-white'>New user account created</p>";
+                    $retrieve_userid = $pdo->prepare("SELECT user_id FROM users WHERE username = :username;");
+                    $retrieve_userid->execute( ["username"=> $new_username]);
+                    $userid = $retrieve_userid->fetchColumn(); // fetchColumn() gets the first column of the first row
+                    $_SESSION["user_id"] = $userid;
+                    $_SESSION["username"] = $new_username;
+                    include "base-top.php";
+                    echo '<h1 class="main-text">Registration Complete</h1>';
+                    echo '<p class="main-text">Welcome to your new account. Please click the button below to access the MedHub</p>';
+                    echo '<section class="d-flex justify-content-center mt-5">';
+                    echo '<a class="blue-button me-3" href="medhub.php">MedHub</a>';
+                    echo '</section>';
                 }   
             }
         }
         else {
+            include "base-top.php";
             echo "<p>Username field missing</p>";
         }
     }
     else {
+        include "base-top.php";
         echo "<p>No form data detected</p>";
     }
 } catch (PDOException $e) {
