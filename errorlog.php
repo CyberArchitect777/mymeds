773e3b04146820149eec98432df5ec605cdab99a
CyberArchitect777 <169656116+CyberArchitect777@users.noreply.github.com>
2024-11-03 13:45:50 +0000

    chore: remove errorlog.php file as no longer need

--- errorlog.php
+++
@@ -1,9 +0,0 @@
-<?php 
-
-function writeConsole($message) {
-    echo "<script>console.log('" . $message . "');</script>";
-}
-
-function writeAlert($message) {
-    echo "<script>alert('" . $message . "');</script>";
-}
